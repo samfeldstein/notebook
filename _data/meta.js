2fa16942dbf5a63d314630ad36b517779c3cc6e0
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-09-18 17:54:01 -0600

    Add some stuff to the head and test wikilinks

--- _data/meta.js
+++ _data/meta.js
@@ -1,5 +1,6 @@
 // https://www.11ty.dev/docs/languages/webc/#using-template-syntax-to-generate-content
 
 export const site = {
-  title: "11ty Foam Template",
+  title: "Eleventy Foam Template",
+  description: "An Eleventy site that works with the Foam VS Code extension.",
 };
