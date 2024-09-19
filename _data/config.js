a66075fdfe06f98fe86e18601167e54b683421cb
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-09-19 11:11:52 -0600

    Build
    Environment variables appear to be working. Don't know that setting an explicit environment variable is necessary in global data, but commented out just in case.

--- _data/config.js
+++ _data/config.js
@@ -1,12 +1,13 @@
 // https://www.11ty.dev/docs/languages/webc/#using-template-syntax-to-generate-content
 
-export default function () {
-  return {
-    // https://www.11ty.dev/docs/environment-vars/#setting-your-own
-    // Terminal command: PROD_MODE=production npx @11ty/eleventy
-    environment: process.env.PROD_MODE || "development",
-  };
-}
+// export default function () {
+//   return {
+//     // I don't THINK this is necessary, but leaving it here for now just in case.
+//     // https://www.11ty.dev/docs/environment-vars/#setting-your-own
+//     // We set this variable with this terminal command: PROD_MODE=production npx @11ty/eleventy // You may need to do this
+//     environment: process.env.PROD_MODE || "development",
+//   };
+// }
 
 export const site = {
   title: "Eleventy Foam Template",
