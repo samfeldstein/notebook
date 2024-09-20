61d61ca6700f966c82df91301c9a92322b1d0e4d
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-09-20 09:23:59 -0600

    Remove <base> and rebuild
    Base seems to be unneccessary after markdown-it plugin.

--- _data/config.js
+++ _data/config.js
@@ -1,21 +1,22 @@
-let siteUrl = "https://eleventy-foam-template.netlify.app/";
+// let siteUrl = "https://eleventy-foam-template.netlify.app/";
 
 // Set url for development environment. If this isn't set, relative links won't work in development.
 // Also see: https://www.11ty.dev/docs/environment-vars/#eleventy-supplied
-if (process.env.ELEVENTY_RUN_MODE !== "build") {
-  siteUrl = "http://localhost:8080/";
-}
+// MAY BE REDUNDANT NOW GIVEN MARKDOW-IT PLUGIN FOR RELATIVE LINKS
+// if (process.env.ELEVENTY_RUN_MODE !== "build") {
+//   siteUrl = "http://localhost:8080/";
+// }
 
 // Information about your site (required)
 export const site = {
   title: "Eleventy Foam Template",
   description: "An Eleventy site that works with the Foam VS Code extension.",
-  url: siteUrl,
+  url: "https://eleventy-foam-template.netlify.app/",
 };
 
 // Information about you (optional)
 export const author = {
-  name: "",
-  email: "",
-  url: "",
+  name: "Sam Feldstein",
+  email: "sam@samfeldstein.xyz",
+  url: "https://samfeldstein.xyz",
 };
