3b562ffe0278d0200c23d5ffd693874b560d2d81
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-10-11 19:20:41 -0600

    Add created and updated dates

--- .eleventy.js
+++ .eleventy.js
@@ -101,7 +101,7 @@ export default function (eleventyConfig) {
 
   // Readable Dates
   eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toUTC().toLocaleString(DateTime.DATE_FULL);
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
   });
 
   // Return all the tags used in a collection
